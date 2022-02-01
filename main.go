44e6d71e98d05a330d7e1ebd0db3813d0b143fab
unknown <odinkh@stud.ntnu.no>
2022-02-01 14:41:29 +0100

    Gin framework

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	//"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+/**func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./Tailwind/html/")))
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
+*/
+
+func main() {
+	r := gin.Default()
+    r.LoadHTMLFiles("./Tailwind/html/index.html")
+	
+
+	r.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.html", gin.H{
+
+		})
+	})
+	log.Fatal(r.Run())
+	}
