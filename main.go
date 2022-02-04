e66ffcf480d294e6646dec3b2ade0067089b7f50
jonakj <jonakj@stud.ntnu.no>
2022-02-04 16:16:28 +0100

    Gin can now serve the static files, and has a temporary requesthandler.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
 	//"net/http"
 	"github.com/gin-gonic/gin"
 )
@@ -16,13 +18,37 @@ import (
 
 func main() {
 	r := gin.Default()
-    r.LoadHTMLFiles("./Tailwind/html/index.html")
-	
+	r.LoadHTMLGlob("./Tailwind/html/*.html")
+	// CSS files
+	r.Static("/dist", "./Tailwind/dist")
+	// Images
+	r.Static("/img", "./Tailwind/img")
 
-	r.GET("/", func(c *gin.Context){
-		c.HTML(http.StatusOK, "index.html", gin.H{
-
-		})
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
-	log.Fatal(r.Run())
+
+	// Generic get request, gets parsed in the RequestHandler function
+	r.GET("/:url", func(c *gin.Context) {
+		url := c.Param("url")
+		RequestHandler(url, c)
+	})
+
+	log.Fatal(r.Run(":80"))
+}
+
+func RequestHandler(url string, c *gin.Context) {
+	fmt.Println("URL IS: " + url + ".")
+	if url == "/" {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	} else {
+		// TODO: Add a validity test here for the url
+
+		// TODO: Remove trailing slashes and .*
+
+		// TODO: Implement templating? Gin has built in template functionality
+
+		// Display the webpage
+		c.HTML(http.StatusOK, url, gin.H{})
 	}
+}
