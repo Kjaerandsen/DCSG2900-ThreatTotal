d2e3683abc223880de65764e2e51a5c3dd7c34f5
jonakj <jonakj@stud.ntnu.no>
2022-02-20 20:59:26 +0100

    Added investigate page

--- threat-total/src/App.js
+++ threat-total/src/App.js
@@ -2,6 +2,7 @@ import './output.css';
 import './App.css';
 import Upload from './upload'
 import Indextest from './indextest';
+import Investigate from './investigate';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
 		<Route path='/' element={<Indextest />} />
     <Route path='/upload' element={<Upload />} />
+    <Route path='/investigate' element={<Investigate />} />
 	  </Routes>
 	</Router>
   );
