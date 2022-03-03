a7e8b2f00304cacd6bcc61d1e9fd61249eeab67d
jonakj <jonakj@stud.ntnu.no>
2022-03-03 14:38:40 +0100

    Added explanation to the input on the search page

--- threat-total/src/indextest.js
+++ threat-total/src/indextest.js
@@ -5,9 +5,6 @@ import ttLogo from './img/TT.png'
 import App from './App';
 
 class Indextest extends React.Component {
-//const Indextest = () => {
-
-    
 
   search(e){
       e.preventDefault();
@@ -16,16 +13,15 @@ class Indextest extends React.Component {
     const re = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
 
     formData.forEach((value, key) => object[key] = value);
+    // Also check if empty and return an error, or invalid on both hash and url
     if (object["inputText"].match(re) ) {
         // if user input matches regex, send to url
         window.location.replace("/upload?url="+encodeURIComponent(object["inputText"]))
-    }
-    else {
+    } else {
         // if user input does not match regex, send to backend and do checks there
         window.location.replace("/upload?hash="+encodeURIComponent(object["inputText"]))
     }
-
-    }
+  }
 
 render(){
     return (
@@ -41,11 +37,19 @@ render(){
         </div>
 		
 		<MainInput />
-		
+
         <form className='w-full container'>
         <div className= "container w-full mt-3 mb-1.5 pl-2 pr-2 sm:pl-36 sm:pr-36 overflow-hidden">
+            <p className="text-left ml-3">
+                Input a <u title="A domain, for example: ntnu.edu">domain</u>,&nbsp;
+                <u title="A webpage, for example: https://www.ntnu.edu/contact">url</u>
+                &nbsp;or a&nbsp;
+                <u title="sha256 hash of a file">file hash</u>:
+            </p>
             <div className="p-0 m-2 border-2 border-gray-400 rounded-lg">
-            <input  className="w-full rounded h-14 p-2 m-0 hover:bg-blue-200" placeholder="https://www.ntnu.no" type="text" name="inputText"></input>
+            <input className="w-full rounded h-14 p-2 m-0 hover:bg-blue-200" placeholder="https://www.ntnu.no"
+                    type="text" name="inputText">
+            </input>
             </div>
         </div>
 
@@ -57,9 +61,7 @@ render(){
         </form>
     </div>
 	</>
-  );
-    }
-//};
+    );}
 }
 
 export default Indextest;
