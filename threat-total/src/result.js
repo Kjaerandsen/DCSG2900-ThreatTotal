d00b229d8119ab79a7a107717b2a14881b55cc5e
jonakj <jonakj@stud.ntnu.no>
2022-03-09 14:28:24 +0100

    Result page is now a function, prints out url parameters.

--- threat-total/src/result.js
+++ threat-total/src/result.js
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useEffect} from "react";
 import Navbar from "./navbar";
+import { useParams } from "react-router-dom";
 
 // look in the url, url decode and write to client
 
-const Result = () => {
-	return (
-	<>
-    <div class="grid place-items-center">
+function Result() {
     
-        <Navbar />
+    const queryParams = new URLSearchParams(window.location.search);
+    const hash = queryParams.get('hash');
+    const url = queryParams.get('url');
 
+    useEffect(() => {
+        console.log({hash})
+        console.log({url})
+    });
 
-    <div class = "bg-red-500 container text-center break-words sm:justify-center">
-        <h1 class="text-3xl font-bold p-0 mt-8 mb-8 sm:mt-12 sm:mb-12 w-auto">
-            Results
-        </h1>
-        <p class ="w-auto green-400">
-            
-            display results here
-        </p>
-    </div>
+    return (
+        <>
+        <div className="grid place-items-center">
         
-    <div class = "bg-green-300 container w-full mt-1.5 mb-3 sm:pl-36 sm:pr-36 flex justify-center overflow-hidden">
-        <a href="./investigate">
-            <button class="bg-orange-500 p-2 rounded justify-center">Submit for Manual Analysis</button>
-        </a>
-    </div>
+            <Navbar />
 
-  </div> 
-  </>
-  );
-};
+
+        <div className="bg-red-500 container text-center break-words sm:justify-center">
+            <h1 className="text-3xl font-bold p-0 mt-8 mb-8 sm:mt-12 sm:mb-12 w-auto">
+                Results
+            </h1>
+            <p className="w-auto green-400">
+                
+                display results here
+            </p>
+        </div>
+            
+        <div className= "bg-green-300 container w-full mt-1.5 mb-3 sm:pl-36 sm:pr-36 flex justify-center overflow-hidden">
+            <a href="./investigate">
+                <button className="bg-orange-500 p-2 rounded justify-center">Submit for Manual Analysis</button>
+            </a>
+        </div>
+
+        </div> 
+        </>
+        );
+}
 
 export default Result;
