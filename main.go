9b16ce2efd5a2267a31ab20f5d074ecf79c7399f
unknown <odinkh@stud.ntnu.no>
2022-03-17 17:23:03 +0100

    Alienvault OTX Api fikset og lagt inn i funksjon

--- main.go
+++ main.go
@@ -20,6 +20,37 @@ import (
 	//"google.golang.org/api/option"
 )
 
+func callAlienVaultAPI(url string) (respone string){
+	content, err := ioutil.ReadFile("./APIKey/OTXapikey.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Convert []byte to string and print to screen
+		APIKey := string(content)
+
+		getURL := "https://otx.alienvault.com//api/v1/indicators/url/"+url+"/general"
+
+		req, err := http.NewRequest("GET", getURL, nil)
+		req.Header.Set("X-OTX-API-KEY", APIKey)
+
+		fmt.Println(req.Header)
+
+		client := &http.Client{}
+
+		res, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
+
+		body, _ := ioutil.ReadAll(res.Body)
+
+		respone=string(body)
+
+	return
+}
+
 func callGoogleApi(url string) (response string){
 	
 	content, err := ioutil.ReadFile("./APIKey/Apikey.txt")
@@ -172,7 +203,11 @@ func main() {
 
 		safebrowserResponse := callGoogleApi(url)
 
+		otxAlienVaultRespone := callAlienVaultAPI(url)
+
 		fmt.Println("safebrowser response::",safebrowserResponse)
+
+		fmt.Println("ALIENVAULT RESPONSE:::", otxAlienVaultRespone)
 		
 		
 
