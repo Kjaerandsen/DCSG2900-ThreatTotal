2edd35fbe017cffc76aa05827a3a815f39dc5340
jonakj <jonakj@stud.ntnu.no>
2022-03-23 17:48:04 +0100

    Added error handling for empty input

--- threat-total/src/components/sources.js
+++ threat-total/src/components/sources.js
@@ -5,27 +5,36 @@ import Source from './source.js'
 // inspired by https://www.youtube.com/watch?v=9C85o8jIgUU
 export default function Sources(props) {
 
+if (props.sourceData[0] === "") {
+    console.log("true")
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Source data:</h1>
+            <p>Error connecting to backend, please try refreshing the page in a couple of minutes.</p><br></br>
+        </div>
+    )
+} else {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Source data:</h1>
+            <div className='bg-yellow-500 bg-red-600 bg-green-600'></div>
+            <div className='grid grid-cols-1 p-2 md:grid-cols-2 xl:grid-cols-3'>
+            {props.sourceData.map((Data, index ) => {
+                if (Data.status === "Safe") {
+                    BG = "bg-green-600"
+                } else if (Data.status === "Risk") {
+                    BG = "bg-red-600"
+                } else {
+                    BG = "bg-yellow-500"
+                }
+                return (
+                <Source Data = {Data} key = {index} BG = {BG}/>
+                )
+            })}
+            </div>
+        </div>
+    );
+    }
+}
 var BG = ""
     
-return (
-    <div>
-        <h1 className="text-2xl font-bold">Source data:</h1>
-        <div className='bg-yellow-500 bg-red-600 bg-green-600'></div>
-        <div className='grid grid-cols-1 p-2 md:grid-cols-2 xl:grid-cols-3'>
-        {props.sourceData.map((Data, index ) => {
-            if (Data.status === "Safe") {
-                BG = "bg-green-600"
-            } else if (Data.status === "Risk") {
-                BG = "bg-red-600"
-            } else {
-                BG = "bg-yellow-500"
-            }
-            return (
-            <Source Data = {Data} key = {index} BG = {BG}/>
-            )
-        })}
-        </div>
-    </div>
-);
-    
-}
