2c26dfd896c36acdfd573af95d2c533cd7dce3d2
unknown <odinkh@stud.ntnu.no>
2022-03-27 22:51:21 +0200

    Hybrid Analysis support for URL scan i VT og Urlscan[.]io.

--- main.go
+++ main.go
@@ -22,14 +22,62 @@ import (
 )
 
 
-func CheckURLHybridAnalyis(url string) (response string){
+func CheckURLHybridAnalyis(URL string) (response string){
 
+	fmt.Println("HYBRID URL: ", URL)
 	//DENNE FUNKSJONENE KAN SCANNE EN URL MEN DETTE BENYTTER SEG AV VIRUS TOTAL/ DETTE ER KANSKJE EN GOD WORK AROUND FOR Å KUNNE BRUKE VT GRATIS SIDEN Hybrid Analysis har lisens.
 	//Problem her kan være at dette må inkomporere en "await - 5-15 sekunder om det ikke er noe cachet result på VirusTotal, fordi den maa kjore ny request.".
 	//Titter på dette. 
 	//Vi har CAP på 2000 request i timen hos Hybrid Analyis, dette burde vell holde??? - 200 max i minuttet. 
 	// https://www.hybrid-analysis.com/docs/api/v2#/Quick%20Scan/post_quick_scan_url Dokumentasjon for dette API endpointet.
-	return
+
+	content, err := ioutil.ReadFile("./APIKey/HybridAnalysisAPI.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Convert []byte to string and print to screen
+    APIKey := string(content)
+
+    postURL := "https://www.hybrid-analysis.com/api/v2/quick-scan/url"
+
+    data := url.Values{}
+    data.Set("scan_type", "all")
+    data.Set("url", URL)
+    data.Set("no_share_third_party","true")
+    data.Set("allow_community_access","false")
+    //data.Set("submit_name","")
+
+    req, err := http.NewRequest("POST", postURL, strings.NewReader(data.Encode()))
+    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+    req.Header.Set("api-key", APIKey)
+    req.Header.Set("User-Agent", "Falcon Sandbox")
+
+    client := &http.Client{}
+
+    res, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer res.Body.Close()
+
+	//res.Body.Read("finished") Her skal jeg føre en sjekk som sjekker om "finished = true eller false"
+
+	//Hvis denne er false skal den vente 5 sekunder og kjøre requesten på nytt.
+	//Eventuelt om det er en måte å ikke close requesten før den er finished???????
+	
+
+	//Her kan det sjekkes om VirusTotal - Status er Malicious og om Urlscan.io - status er malicious, suspicious, clean etc. også bare returnere denne responsen. 
+
+
+    //fmt.Println("response Status:", res.Status)
+    //fmt.Print("Response Headers:", res.Header)
+    body, _ := ioutil.ReadAll(res.Body)
+    //fmt.Println("response Body:", string(body))
+    response = string(body)
+
+    return
+	
 }
 
 
@@ -315,6 +363,12 @@ func main() {
 		ResultHybridA:=CheckFileHashHybridAnalysis(filehashHybrid);
 
 		fmt.Println("\n\n\n\n\n HYBRID ANALYSIS!!!!::::::::!!!\n\n\n",ResultHybridA);
+
+		HybridTestURL := "https://testsafebrowsing.appspot.com/s/malware.html"
+
+		ResultURLHybridA:=CheckURLHybridAnalyis(HybridTestURL)
+
+		fmt.Println("\n\n\n\n\n HYBRID URL:\n\n", ResultURLHybridA)
 
 	})
 	/*
