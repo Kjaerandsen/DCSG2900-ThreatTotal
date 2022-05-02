07e6a829c13999f558986b6e8eb2fa92893a6b45
jonakj <jonakj@stud.ntnu.no>
2022-05-02 16:44:27 +0200

    Oauth2 work

--- utils/global.go
+++ utils/global.go
@@ -1,5 +1,17 @@
 package utils
 
-import "github.com/gomodule/redigo/redis"
+import (
+	"context"
+
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/gomodule/redigo/redis"
+	"golang.org/x/oauth2"
+)
 
 var Conn redis.Conn
+var Provider *oidc.Provider
+var Ctx context.Context
+
+// https://pkg.go.dev/golang.org/x/oauth2@v0.0.0-20200107190931-bf48bf16ab8d?utm_source=gopls#example-Config
+// https://github.com/coreos/go-oidc/blob/v3/example/userinfo/app.go
+var Config oauth2.Config
