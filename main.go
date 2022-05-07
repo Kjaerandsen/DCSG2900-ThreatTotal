5e276fd9bbd89e99d2fa29d058cda3ed64b5f51a
jonakj <jonakj@stud.ntnu.no>
2022-05-07 12:49:08 +0200

    Moved retireval of uploaded file data to the api package

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"dcsg2900-threattotal/utils"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -220,142 +219,14 @@ func main() {
 
 	r.GET("/upload", func(c *gin.Context) {
 
-		// VT key has been added. REMEMBER TO DEACTIVATE AND CHANGE BEFORE FINAL RELEASE.
-		// prepare request towards API
+		hash := c.Query("userAuth")
 
-		content, err := ioutil.ReadFile("./APIKey/virusTotal.txt")
-		if err != nil {
-			//log.Fatal(err)
-			fmt.Println(err)
-			logging.Logerror(err)
-		}
-		// Convert []byte to string and print to screen
-		APIKey := string(content)
-		// remember to change api key, and reference it to a file instead
-		// as well as deactivate the key from the account, as it's leaked.
-
-		// fetch based on url parameter, file = id
-		id := c.Query("file")
-		log.Println(id)
-		if id == "" {
-			log.Println("error, ID is empty")
-			logging.Logerrorinfo("Error, ID is empty - Upload")
-		}
-
-		url := fmt.Sprintf("https://www.virustotal.com/api/v3/files/%s", id)
-		log.Println(url)
-		log.Println(id)
-
-		vtReq, _ := http.NewRequest("GET", url, nil)
-
-		vtReq.Header.Add("Accept", "application/json")
-
-		vtReq.Header.Add("X-Apikey", APIKey)
-
-		vtRes, _ := http.DefaultClient.Do(vtReq)
-
-		//defer vtRes.Body.Close()
-
-		vtBody, _ := ioutil.ReadAll(vtRes.Body)
-
-		log.Println(string(vtBody))
-
-		var vtResponse utils.FileUploadData2
-
-		unmarshalledBody := json.Unmarshal(vtBody, &vtResponse)
-
-		if unmarshalledBody != nil {
-			log.Println(unmarshalledBody)
-		}
-
-		/*
-			test2 := reflect.ValueOf(vtResponse.Data.Attributes.LastAnalysisResults)
-			lengthOfTest2 := test2.NumField() */
-
-		var test3 = make([]utils.FrontendResponse4, len(vtResponse.Data.Attributes.LastAnalysisResults))
-
-		//var test [4]utils.FrontendResponse4
-
-		log.Println("here is the test output we maybe want")
-		i := 0
-
-		// TODO for later, remove teststruct, as it's only used to put into totalverdict, later
-		var testStruct = make([]utils.FrontendResponse2, len(vtResponse.Data.Attributes.LastAnalysisResults))
-
-		// iterate through results
-		for _, val := range vtResponse.Data.Attributes.LastAnalysisResults {
-			//log.Printf("testing, %s, %s", key, val)
-			// initialize struct
-			test3[i] = val
-			// print
-			log.Println(test3[i])
-
-			// save engine name
-			testStruct[i].ID = i + 1
-			testStruct[i].SourceName = test3[i].EngineName
-			// resolution of AV
-			testStruct[i].EN.Status = test3[i].Category
-
-			testStruct[i].EN.Content = vtResponse.Data.Attributes.MeaningfulName
-			testStruct[i].EN.Description = vtResponse.Data.Attributes.Magic
-			testStruct[i].EN.Tags = vtResponse.Data.Attributes.TypeTag
-
-			//testStruct.EN.Description =
-
-			// can also display the total status (last analysis stats)
-			// this is an int ^^ so cant fill it in frontendresponse2
-			// question is, do we do it here or later
-
-			i++
-		}
-		log.Println(testStruct)
-
-		var totalVerdict utils.ResultFrontendResponse
-		totalVerdict.FrontendResponse = testStruct
-		// IMPORTANT TODO, FIGURE ROUTING
-
-		// Possible to add more cases in the future, for more accurate assessements
-		// very realisitc that we need more cases, too narrow for accurate results per now.
-		if vtResponse.Data.Attributes.LastAnalysisStats.Malicious == 0 && vtResponse.Data.Attributes.LastAnalysisStats.Suspicious == 0 {
-			totalVerdict.EN.Result = "File is safe."
-			// osv totalVerdict.EN.Result = fmt.Sprintf("File is considered safe", x av y)
-		} else if vtResponse.Data.Attributes.TotalVotes.Malicious > 0 && vtResponse.Data.Attributes.LastAnalysisStats.Suspicious >= 0 {
-			totalVerdict.EN.Result = "File has malicious indicators, consider escalating. "
-		} else if vtResponse.Data.Attributes.LastAnalysisStats.Harmless > 0 && vtResponse.Data.Attributes.LastAnalysisStats.Malicious == 0 {
-			totalVerdict.EN.Result = "File has been confirmed benign."
+		authenticated, _ := auth.Authenticate("", hash)
+		if !authenticated {
+			c.JSON(http.StatusUnauthorized, gin.H{"authenticated": "You are not authenticated. User login is invalid."})
 		} else {
-			totalVerdict.EN.Result = "File is suspicious."
+			api.UploadFileRetrieve(c)
 		}
-
-		var engines int = len(vtResponse.Data.Attributes.LastAnalysisResults)
-
-		utils.SetResultFile(&totalVerdict, engines)
-
-		log.Println("look here")
-		log.Println(totalVerdict)
-
-		fmt.Println(totalVerdict)
-
-		// return here
-
-		fileInt, err := json.Marshal(totalVerdict)
-		if err != nil {
-			fmt.Println(err)
-			//c.Data(http.StatusInternalServerError, "application/json", )
-		}
-
-		//fmt.Println("WHERE IS MY CONTENT", responseData)
-
-		c.Data(http.StatusOK, "application/json", fileInt)
-
-		// sort list based on what is malicious
-		//log.Print(test3)
-
-		// hent resultat via cache
-
-		// total votes feltet virker relevant
-		// LAST ANALYSIS STATS - MALICIOUS
-
 	})
 
 	/**
