ca68d519b39d273136af96d6c69aea15a034e825
jonakj <jonakj@stud.ntnu.no>
2022-05-07 13:57:19 +0200

    Added caching to the 'hash-intelligence' endpoint.

--- api/hash-intelligence.go
+++ api/hash-intelligence.go
@@ -13,9 +13,66 @@ import (
 
 func HashIntelligence(c *gin.Context) {
 
-	var wg sync.WaitGroup
+	var hashInt []byte
+	var err error
+
 	hash := c.Query("hash")
 
+	value, err := utils.Conn.Do("GET", hash)
+	if value == nil {
+		if err != nil {
+			fmt.Println("Error:" + err.Error())
+		}
+		fmt.Println("No Cache hit")
+
+		// Perform the request
+		hashInt, err = hashSearch(hash)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error ": "Invalid response from third party API's."})
+			return
+		}
+
+		// Add the data to the database
+		response, err := utils.Conn.Do("SETEX", hash, 300, hashInt)
+		if err != nil {
+			fmt.Println("Error adding data to redis:" + err.Error())
+		}
+
+		fmt.Println(response)
+
+	} else {
+
+		fmt.Println("Cache hit")
+		responseBytes, err := json.Marshal(value)
+		if err != nil {
+			fmt.Println("Error handling redis response:" + err.Error())
+			http.Error(c.Writer, "Failed retrieving api data.", http.StatusInternalServerError)
+			return
+			// Maybe do another call to delete the key from the database?
+		}
+		/**
+		//var checkData utils.ResultFrontendResponse
+		err = json.Unmarshal(responseBytes, &checkdata)
+		if err!=nil {
+			fmt.Println(string(checkData))
+		}
+		fmt.Println(string(checkData))
+		*/
+		err = json.Unmarshal(responseBytes, &hashInt)
+		if err != nil {
+			fmt.Println("Error handling redis response:" + err.Error())
+			http.Error(c.Writer, "Failed retrieving api data.", http.StatusInternalServerError)
+			return
+			// Maybe do another call to delete the key from the database?
+		}
+	}
+
+	c.Data(http.StatusOK, "application/json", hashInt)
+}
+
+func hashSearch(hash string) (data []byte, err error) {
+
+	var wg sync.WaitGroup
 	var responseData [2]utils.FrontendResponse2
 
 	var hybridApointer, AlienVaultpointer *utils.FrontendResponse2
@@ -35,15 +92,12 @@ func HashIntelligence(c *gin.Context) {
 
 	utils.SetResultHash(resultPointer, len(responseData))
 
-	Hashint, err := json.Marshal(resultResponse)
+	hashInt, err := json.Marshal(resultResponse)
 	if err != nil {
 		fmt.Println(err)
 		logging.Logerror(err)
-		//c.Data(http.StatusInternalServerError, "application/json", )
+		return nil, err
 	}
 
-	//fmt.Println("WHERE IS MY CONTENT", responseData)
-
-	c.Data(http.StatusOK, "application/json", Hashint)
-
+	return hashInt, nil
 }
