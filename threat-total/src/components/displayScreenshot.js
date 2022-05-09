0ea6fc3808c17144bf86daec7799ed3deb1648d0
jonakj <jonakj@stud.ntnu.no>
2022-05-09 15:13:57 +0200

    Empty display if no image is provided

--- threat-total/src/components/displayScreenshot.js
+++ threat-total/src/components/displayScreenshot.js
@@ -2,16 +2,10 @@ import React from 'react';
 
 
 export default function Screenshot(screenshot) {
-
-    //i18next.init({ resources: {} });
-    //i18next.addResourceBundle('en', 'sourceEn', translationData.en);
-    //i18next.addResourceBundle('no', 'sourceNo', translationData.no);
     
-    // Add the translation data from the backend
 if (screenshot.length !== 0) {
 
 var base64Image = 'data:image/png;base64,'+screenshot.screenshot;
-    console.log(screenshot)
     
     return(
         <div className='bg-white border-2 m-2 border-gray-400 rounded-lg p-1 text-left'>
@@ -33,21 +27,7 @@ var base64Image = 'data:image/png;base64,'+screenshot.screenshot;
     
 }else{
     return(
-        <div className='bg-white border-2 m-2 border-gray-400 rounded-lg p-1 text-left'>
-            <div className='flex'>
-                <div className='border-r-2 grid place-items-center pr-1'>
-                <img src={base64Image} alt="Screenshot" />
-                </div>
-                <div className="p-1"> 
-                    <br></br>
-                    <br></br>
-                </div>
-            </div>
-            <div className="">
-                <br></br>
-                <br></br>
-            </div>   
-        </div>
+        <></> 
     )
 }
 }
